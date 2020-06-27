8b8fd7daaba54a3b4a51ee4ad1dc9ab2db47cfe4
Hermes J. Pappas <hjp@hjp.gr>
2020-06-27 07:06:45 +0300

    Change to grid side size (always a square), implement hover painting functionality

--- etch.js
+++ etch.js
@@ -1,17 +1,26 @@
-const rows = 16;
-const columns = 16;
+let gridside = 16;
 
+//fill our grid using javascript
 const gridcont = document.querySelector('#grid-container');
 const gridstyle = getComputedStyle(gridcont);
-gridcont.style.gridTemplateColumns = repStrWhile(`${100/columns}% `, columns);
-gridcont.style.gridTemplateRows = repStrWhile(`${100/rows}% `, rows);
+gridcont.style.gridTemplateColumns = repStrWhile(`${100/gridside}% `, gridside);
+gridcont.style.gridTemplateRows = repStrWhile(`${100/gridside}% `, gridside);
 
-console.log(gridstyle.gridTemplateColumns);
-console.log(gridstyle.gridTemplateRows);
-for(let i = 0; i < (rows * columns); i++)
+for(let i = 0; i < (gridside * gridside); i++)
 {
-	const griditem = document.createElement('div');
+	const griditem = document.createElement("div");
 	griditem.classList.add("grid-item");
-	griditem.textContent = i+1;
+	//griditem.textContent = i+1;
 	gridcont.appendChild(griditem);
 }
+
+//make it so that when a grid box is hovered over it changes color
+const gridsquares = document.querySelectorAll(".grid-item");
+gridsquares.forEach((gridsquare) => 
+{
+	gridsquare.addEventListener('mouseover', (e) => 
+	{
+		gridsquare.style.backgroundColor = "#ccc";
+		// griditem.classList.add('touched');
+	});
+});
